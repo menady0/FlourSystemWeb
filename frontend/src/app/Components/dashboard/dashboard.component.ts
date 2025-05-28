db32b94a5ee2a251aab903d2e76afdeffcf4d68b
menady0 <menady20@gmail.com>
2025-05-28 23:43:49 +0300

    added download desktop app

--- frontend/src/app/Components/dashboard/dashboard.component.ts
+++ frontend/src/app/Components/dashboard/dashboard.component.ts
@@ -4,15 +4,19 @@ import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
 import { PageTitleService } from '../../core/services/page-title.service';
 import { filter } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet, HeaderComponent, NavComponent],
+  imports: [RouterOutlet, HeaderComponent, NavComponent, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
-  constructor(private router: Router, private pageTitleService: PageTitleService) {}
+  desktopAppUrl: string | null = null;
+
+  constructor(private router: Router, private pageTitleService: PageTitleService, private http: HttpClient) {}
 
   ngOnInit() {
     this.router.events.pipe(
@@ -25,5 +29,12 @@ export class DashboardComponent implements OnInit {
       else if (url.includes('settings')) title = 'الإعدادات';
       this.pageTitleService.setTitle(title);
     });
+
+        const jsonUrl = 'https://raw.githubusercontent.com/menady0/FlourSystem/main/update-info.json';
+        this.http.get<{url: string}>(jsonUrl).subscribe({
+          next: (data) => {
+            this.desktopAppUrl = data.url
+          }
+        })
   }
 }
